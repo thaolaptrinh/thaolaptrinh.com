c76a8602b1093f5aa1a29d86e5b7252276dace17
thaolaptrinh <thaolaptrinh@gmail.com>
2025-10-29 12:05:20 +0000

    Deploying to gh-pages from @ thaolaptrinh/thaolaptrinh.com@4697046f3c14023055b788048e3b9bb625a22174 🚀

---
+++ _nuxt/BCXXrkw2.js
@@ -0,0 +1,1 @@
+import{r as n}from"./CtelIHom.js";import a from"./DP4wmMSs.js";import{y as s,K as t,h as e,w as i,I as p}from"./BLrWm5S7.js";import"./iik6CYzq.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
